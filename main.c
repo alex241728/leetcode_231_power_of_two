132917733bca990bb82f452ea5f3db8d79600815
alex241728 <alex_241728@163.com>
2024-03-14 12:08:43 -0400

    loop method

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+bool isPowerOfTwo(int n);
+
+int main() {
+    printf("%d\n", isPowerOfTwo(1));
+
+    printf("%d\n", isPowerOfTwo(16));
+
+    printf("%d\n", isPowerOfTwo(3));
+
+    return 0;
+}
+
+bool isPowerOfTwo(int n) {
+    long long num = 1;
+    while (true) {
+        if (num == n) {
+            return true;
+        }
+
+        if (num > n) {
+            break;
+        }
+
+        num *= 2;
+  }
+
+    return false;
+}
